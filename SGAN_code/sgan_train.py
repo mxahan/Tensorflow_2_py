fae737a26cac9995d895b19a4b7768e0c00003e2
mxahan <smxahsan@gmail.com>
2020-05-08 03:08:39 -0400

    new

---
+++ SGAN_code/sgan_train.py
@@ -0,0 +1,38 @@
+#%% Load library
+
+
+
+
+#%% Load Data and prepare data
+
+#%% hyperparameters
+
+#%% Loss function
+
+
+#%% Model training + Optimization
+
+#%% Model Definition
+
+
+#%% Model loading and main function to train
+
+
+#%% Model save 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
